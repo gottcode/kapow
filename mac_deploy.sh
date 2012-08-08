35cf1625501c08b8440b60b19b33cc89c3c2fb4d
Graeme Gott <graeme@gottcode.org>
2012-08-08 18:24:23 -0400

    Tag version 1.4.2

--- mac_deploy.sh
+++ mac_deploy.sh
@@ -2,7 +2,7 @@
 
 APP='Kapow'
 BUNDLE="$APP.app"
-VERSION='1.4.1.1'
+VERSION='1.4.2'
 
 # Remove any previous disk folder or DMG
 echo -n 'Preparing... '
--- windows_deploy.bat
+++ windows_deploy.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 SET APP=Kapow
-SET VERSION=1.4.1.1
+SET VERSION=1.4.2
 
 ECHO Copying executable
 MKDIR %APP%
