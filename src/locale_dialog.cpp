691f228822cfa6c7e37427733ed7c77937e936c7
Graeme Gott <graeme@gottcode.org>
2013-09-16 08:30:51 -0400

    FIXED: Setting locale did not change layout direction.

--- src/locale_dialog.cpp
+++ src/locale_dialog.cpp
@@ -21,8 +21,8 @@
 
 #include "settings.h"
 
+#include <QApplication>
 #include <QComboBox>
-#include <QCoreApplication>
 #include <QDialogButtonBox>
 #include <QDir>
 #include <QFile>
@@ -124,6 +124,9 @@ void LocaleDialog::loadTranslator(const QString& name, const QStringList& datadi
 	static QTranslator translator;
 	translator.load(m_appname + current, m_path);
 	QCoreApplication::installTranslator(&translator);
+
+	// Work around bug in Qt 5 where text direction is not loaded
+	QApplication::setLayoutDirection(QLocale(current).textDirection());
 }
 
 //-----------------------------------------------------------------------------
