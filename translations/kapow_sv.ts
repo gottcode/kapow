3a11fad9ecaef896cab75923876a26b8d213dcf8
Graeme Gott <graeme@gottcode.org>
2017-10-27 15:01:58 -0400

    Sync translations with Transifex.

---
+++ translations/kapow_sv.ts
@@ -0,0 +1,486 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv" sourcelanguage="en">
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Välj programmets språk:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;System Language&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Anteckning</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Starta om detta program för att språkändringen ska verkställas.</translation>
+    </message>
+</context>
+<context>
+    <name>Report</name>
+    <message>
+        <source>Time Sheet Report</source>
+        <translation>Tidrapport</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Namn:</translation>
+    </message>
+    <message>
+        <source>Company:</source>
+        <translation>Företag:</translation>
+    </message>
+    <message>
+        <source>Address:</source>
+        <translation>Adress:</translation>
+    </message>
+    <message>
+        <source>Phone:</source>
+        <translation>Telefon:</translation>
+    </message>
+    <message>
+        <source>Fax:</source>
+        <translation>Fax:</translation>
+    </message>
+    <message>
+        <source>Email:</source>
+        <translation>E-post:</translation>
+    </message>
+    <message>
+        <source>Website:</source>
+        <translation>Hemsida:</translation>
+    </message>
+    <message>
+        <source>N/A</source>
+        <translation>N/A</translation>
+    </message>
+    <message>
+        <source>Prepend currency symbol</source>
+        <translation>Inledande valutasymbol</translation>
+    </message>
+    <message>
+        <source>Hourly rate:</source>
+        <translation>Timtaxa:</translation>
+    </message>
+    <message>
+        <source>Tax rate:</source>
+        <translation>Skattesats:</translation>
+    </message>
+    <message>
+        <source>Currency symbol:</source>
+        <translation>Valutasymbol:</translation>
+    </message>
+    <message>
+        <source>Contact Information</source>
+        <translation>Kontaktuppgifter</translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation>Data</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Förhandsgranskning</translation>
+    </message>
+    <message>
+        <source>Export</source>
+        <translation>Exportera</translation>
+    </message>
+    <message>
+        <source>Print</source>
+        <translation>Skriv ut</translation>
+    </message>
+    <message>
+        <source>Web Page (*.html *.htm)</source>
+        <translation>Hemsida (*.html *.htm)</translation>
+    </message>
+    <message>
+        <source>iCalendar (*.ics)</source>
+        <translation>iCalendar (*.ics)</translation>
+    </message>
+    <message>
+        <source>Outlook CSV (*.csv)</source>
+        <translation>Outlook CSV (*.csv)</translation>
+    </message>
+    <message>
+        <source>Export Report</source>
+        <translation>Exportera rapport</translation>
+    </message>
+    <message>
+        <source>View Reports</source>
+        <translation>Visa rapport</translation>
+    </message>
+    <message>
+        <source>Create Report</source>
+        <translation>Skapa rapport</translation>
+    </message>
+    <message>
+        <source>Phone: %1</source>
+        <translation>Telefon: %1</translation>
+    </message>
+    <message>
+        <source>Fax: %1</source>
+        <translation>Fax: %1</translation>
+    </message>
+    <message>
+        <source>Hours</source>
+        <translation>Timmar</translation>
+    </message>
+    <message>
+        <source>Subtotal</source>
+        <translation>Delsumma</translation>
+    </message>
+    <message>
+        <source>Taxes</source>
+        <translation>Skatter</translation>
+    </message>
+    <message>
+        <source>Total</source>
+        <translation>Summa</translation>
+    </message>
+</context>
+<context>
+    <name>Session</name>
+    <message numerus="yes">
+        <source>%n hour(s)</source>
+        <translation>
+            <numerusform>%n timma</numerusform>
+            <numerusform>%n timmar</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>%L1 hours</source>
+        <translation>%L1 timmar</translation>
+    </message>
+</context>
+<context>
+    <name>SessionDelegate</name>
+    <message>
+        <source>Error</source>
+        <translation>Fel</translation>
+    </message>
+    <message>
+        <source>Session conflicts with other sessions.</source>
+        <translation>Sessionskonflikt med andra sessioner.</translation>
+    </message>
+</context>
+<context>
+    <name>SessionDialog</name>
+    <message>
+        <source>Add Session</source>
+        <translation>Lägg till session</translation>
+    </message>
+    <message>
+        <source>Date:</source>
+        <translation>Datum:</translation>
+    </message>
+    <message>
+        <source>Start:</source>
+        <translation>Start:</translation>
+    </message>
+    <message>
+        <source>Stop:</source>
+        <translation>Stopp:</translation>
+    </message>
+    <message>
+        <source>Task:</source>
+        <translation>Uppgift:</translation>
+    </message>
+    <message>
+        <source>Edit Session</source>
+        <translation>Redigera session</translation>
+    </message>
+</context>
+<context>
+    <name>SessionModel</name>
+    <message>
+        <source>Total</source>
+        <translation>Summa</translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation>Datum</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Stopp</translation>
+    </message>
+    <message>
+        <source>Task</source>
+        <translation>Uppgift</translation>
+    </message>
+    <message>
+        <source>Hours</source>
+        <translation>Timmar</translation>
+    </message>
+    <message>
+        <source>Daily</source>
+        <translation>Dagligen</translation>
+    </message>
+    <message>
+        <source>Weekly</source>
+        <translation>Veckovis</translation>
+    </message>
+    <message>
+        <source>Monthly</source>
+        <translation>Månadsvis</translation>
+    </message>
+    <message>
+        <source>Report</source>
+        <translation>Rapport</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>00:00:00</source>
+        <translation>00:00:00</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Stopp</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>&amp;Minimize</source>
+        <translation>&amp;Minimera</translation>
+    </message>
+    <message>
+        <source>&amp;Project</source>
+        <translation>&amp;Projekt</translation>
+    </message>
+    <message>
+        <source>&amp;Add</source>
+        <translation>&amp;Lägg till</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+N</source>
+        <translation>Ctrl+Shift+L</translation>
+    </message>
+    <message>
+        <source>&amp;Remove</source>
+        <translation>&amp;Ta bort</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+Delete</source>
+        <translation>Ctrl+Shift+Delete</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Avsluta</translation>
+    </message>
+    <message>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+A</translation>
+    </message>
+    <message>
+        <source>&amp;Session</source>
+        <translation>&amp;Session</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Redigera</translation>
+    </message>
+    <message>
+        <source>Ctrl+Delete</source>
+        <translation>Ctrl+Delete</translation>
+    </message>
+    <message>
+        <source>S&amp;ettings</source>
+        <translation>Inst&amp;ällningar</translation>
+    </message>
+    <message>
+        <source>Columns</source>
+        <translation>Kolumner</translation>
+    </message>
+    <message>
+        <source>&amp;Decimal Totals</source>
+        <translation>Summa i &amp;decimaltal</translation>
+    </message>
+    <message>
+        <source>&amp;Inline Editing</source>
+        <translation>Radred&amp;igering</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>Programsprå&amp;k</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Hjälp</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Om</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Om &amp;Qt</translation>
+    </message>
+    <message>
+        <source>Project</source>
+        <translation>Projekt</translation>
+    </message>
+    <message>
+        <source>Timer</source>
+        <translation>Timer</translation>
+    </message>
+    <message>
+        <source>Show all</source>
+        <translation>Visa alla</translation>
+    </message>
+    <message>
+        <source>Show only unbilled</source>
+        <translation>Visa endast ofakturerade</translation>
+    </message>
+    <message>
+        <source>Show only this year</source>
+        <translation>Visa endast detta år</translation>
+    </message>
+    <message>
+        <source>Show only this month</source>
+        <translation>Visa endast denna månaden</translation>
+    </message>
+    <message>
+        <source>Show only this week</source>
+        <translation>Visa endast denna veckan</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2008-%1 Graeme Gott</source>
+        <translation>Copyright &amp;copy; 2008-%1 Graeme Gott</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Utgiven under &lt;a href=%1&gt;GPL 3&lt;/a&gt; licens</translation>
+    </message>
+    <message>
+        <source>Session conflicts with other sessions.</source>
+        <translation>Sessionskonflikt med andra sessioner.</translation>
+    </message>
+    <message>
+        <source>&amp;Restore</source>
+        <translation>&amp;Återställ</translation>
+    </message>
+    <message>
+        <source>Unable to read time data.</source>
+        <translation>Kan inte läsa tidsdata.</translation>
+    </message>
+    <message>
+        <source>Untitled</source>
+        <translation>Titel saknas</translation>
+    </message>
+    <message>
+        <source>Unable to write time data.</source>
+        <translation>Kan inte skriva tidsdata.</translation>
+    </message>
+    <message>
+        <source>There are timers running. Stop timers and quit?</source>
+        <translation>Timers är igång. Vill du stoppa timers och avsluta?</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>A program to help track spent time</source>
+        <translation>Ett program för att hjälpa till att spåra spenderad tid</translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation>Fråga</translation>
+    </message>
+    <message>
+        <source>&amp;Create Report...</source>
+        <translation>&amp;Skapa rapport...</translation>
+    </message>
+    <message>
+        <source>View R&amp;eports</source>
+        <translation>&amp;Visa rapporter</translation>
+    </message>
+    <message>
+        <source>Cancel this session?</source>
+        <translation>Avbryta denna session?</translation>
+    </message>
+    <message>
+        <source>Add Project</source>
+        <translation>Lägg till projekt</translation>
+    </message>
+    <message>
+        <source>Enter project name:</source>
+        <translation>Ange projektnamn:</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fel</translation>
+    </message>
+    <message>
+        <source>Unable to create time data location.</source>
+        <translation>Kan inte skapa plats för tidsdata.</translation>
+    </message>
+    <message>
+        <source>Unable to move time data location.</source>
+        <translation>Kan inte flytta plats för tidsdata.</translation>
+    </message>
+    <message>
+        <source>A project with that name already exists.</source>
+        <translation>Ett projekt med det namnet finns redan.</translation>
+    </message>
+    <message>
+        <source>Remove selected project?</source>
+        <translation>Ta bort valt projekt?</translation>
+    </message>
+    <message>
+        <source>Remove selected session?</source>
+        <translation>Ta bort vald session?</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n timer(s) running</source>
+        <translation>
+            <numerusform>%n timer igång</numerusform>
+            <numerusform>%n timers igång</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Kapow Punch Clock</source>
+        <translation>Kapow Stämpelklocka</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Punch clock program</source>
+        <translation>Stämpelklocksprogram</translation>
+    </message>
+    <message>
+        <source>Store settings as INI format in specified file.</source>
+        <translation>Spara inställningar som INI-format i angiven fil.</translation>
+    </message>
+    <message>
+        <source>file</source>
+        <translation>fil</translation>
+    </message>
+    <message>
+        <source>Do not create automatic backups of time data.</source>
+        <translation>Skapa inte automatisk säkerhetskopiering av tidsdata.</translation>
+    </message>
+    <message>
+        <source>The time data file to use.</source>
+        <translation>Tidsdatafil att använda.</translation>
+    </message>
+</context>
+</TS>
