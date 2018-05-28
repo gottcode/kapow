022175ffb2ffa4c5f58891141a4c6f82ffd8cfe7
Graeme Gott <graeme@gottcode.org>
2018-05-28 13:58:51 -0400

    Split script for updating translations.

--- icons/po/update-po.sh
+++ icons/po/update-po.sh
@@ -4,7 +4,6 @@
 echo -n 'Preparing files...'
 cd ..
 
-rm -f kapow.desktop.in.h
 rm -f kapow.desktop.in
 cp kapow.desktop kapow.desktop.in
 sed -e '/^Name\[/ d' \
@@ -14,9 +13,7 @@ sed -e '/^Name\[/ d' \
 	-e 's/^GenericName/_GenericName/' \
 	-e 's/^Comment/_Comment/' \
 	-i kapow.desktop.in
-intltool-extract --quiet --type=gettext/ini kapow.desktop.in
 
-rm -f kapow.appdata.xml.in.h
 rm -f kapow.appdata.xml.in
 cp kapow.appdata.xml kapow.appdata.xml.in
 sed -e '/p xml:lang/ d' \
@@ -29,17 +26,8 @@ sed -e '/p xml:lang/ d' \
 	-e 's/<name>/<_name>/' \
 	-e 's/<\/name>/<\/_name>/' \
 	-i kapow.appdata.xml.in
-intltool-extract --quiet --type=gettext/xml kapow.appdata.xml.in
 
 cd po
-echo ' DONE'
-
-
-echo -n 'Extracting messages...'
-xgettext --from-code=UTF-8 --c++ --keyword=_ --keyword=N_:1 \
-	--package-name='Kapow' --copyright-holder='Graeme Gott' \
-	--output=description.pot ../*.h
-sed 's/CHARSET/UTF-8/' -i description.pot
 echo ' DONE'
 
 
@@ -56,11 +44,10 @@ echo -n 'Merging translations...'
 cd ..
 
 intltool-merge --quiet --desktop-style po kapow.desktop.in kapow.desktop
-rm -f kapow.desktop.in.h
 rm -f kapow.desktop.in
 
 intltool-merge --quiet --xml-style po kapow.appdata.xml.in kapow.appdata.xml
-rm -f kapow.appdata.xml.in.h
+echo >> kapow.appdata.xml
 rm -f kapow.appdata.xml.in
 
 echo ' DONE'
---
+++ icons/po/update-pot.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+
+echo -n 'Preparing files...'
+cd ..
+
+rm -f kapow.desktop.in
+cp kapow.desktop kapow.desktop.in
+sed -e '/^Name\[/ d' \
+	-e '/^GenericName\[/ d' \
+	-e '/^Comment\[/ d' \
+	-e '/^Icon/ d' \
+	-e '/^Keywords/ d' \
+	-i kapow.desktop.in
+
+rm -f kapow.appdata.xml.in
+cp kapow.appdata.xml kapow.appdata.xml.in
+sed -e '/p xml:lang/ d' \
+	-e '/summary xml:lang/ d' \
+	-e '/name xml:lang/ d' \
+	-e '/<developer_name>/ d' \
+	-i kapow.appdata.xml.in
+
+cd po
+echo ' DONE'
+
+
+echo -n 'Extracting messages...'
+xgettext --from-code=UTF-8 --output=description.pot \
+	--package-name='Kapow' --copyright-holder='Graeme Gott' \
+	../*.in
+sed 's/CHARSET/UTF-8/' -i description.pot
+echo ' DONE'
+
+
+echo -n 'Cleaning up...'
+cd ..
+
+rm -f kapow.desktop.in
+rm -f kapow.appdata.xml.in
+
+echo ' DONE'
