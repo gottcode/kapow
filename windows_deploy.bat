493d929481a70c7b4a2ee8030f50aabefe05cd51
Graeme Gott <graeme@gottcode.org>
2020-04-20 20:39:37 -0400

    Correct path in Windows deployment.

--- windows_deploy.bat
+++ windows_deploy.bat
@@ -7,7 +7,7 @@ SET VERSION=1.5.9
 
 ECHO Copying executable
 MKDIR %APP%
-COPY %APP%.exe %APP% >nul
+COPY release\%APP%.exe %APP% >nul
 
 ECHO Copying translations
 SET TRANSLATIONS=%APP%\translations
