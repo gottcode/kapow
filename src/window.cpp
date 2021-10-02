f772f78fc6da07ab61f3eea48d415fd43fb01581
Graeme Gott <graeme@gottcode.org>
2021-10-02 15:52:23 -0400

    Fix clazy warnings detaching range loop

--- src/window.cpp
+++ src/window.cpp
@@ -382,7 +382,7 @@ void Window::closeEvent(QCloseEvent* event)
 
 		// Prompt user about running timers
 		if (QMessageBox::question(this, tr("Question"), tr("There are timers running. Stop timers and quit?"), QMessageBox::Yes | QMessageBox::No, QMessageBox::No) == QMessageBox::Yes) {
-			for (Project* project : m_active_timers) {
+			for (Project* project : qAsConst(m_active_timers)) {
 				project->stop(m_current_time);
 			}
 			m_active_timers.clear();
